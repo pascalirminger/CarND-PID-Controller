0e8319d48109170a30fc1349995c3223047dbf3b
Pascal Irminger <pascal.irminger@gmail.com>
2018-03-04 11:53:48 +0100

    Documentation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,32 +1,32 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    // Initialize coefficients
     this->Kp = Kp;
     this->Ki = Ki;
     this->Kd = Kd;
 
+    // Initialize errors
     p_error = 0.0;
     i_error = 0.0;
     d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    // Differential error, using previous cte (stored in p_error)
     d_error = cte - p_error;
+    // Proportional error
     p_error = cte;
+    // Integral error
     i_error += cte;
 }
 
 double PID::TotalError() {
+    // Calculate the total PID error
     return -Kp * p_error - Ki * i_error - Kd * d_error;
 }
 
